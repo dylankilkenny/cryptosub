042ec882115e82f1c6373e7cb84712c4863389d1
Dylan Kilkenny <dylankilkenny95@gmail.com>
2018-06-17 11:49:57 +0100

    nodejs server

---
+++ server/index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const cors = require('cors')
+const fetch = require('node-fetch')
+var _ = require('lodash');
+var moment = require('moment');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+// app.use(cors())
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+// ---------  Database  ---------- //
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
+let db;
+MongoClient.connect(url, (err, client) => {
+    if (err) return console.log(err)
+    db = client.db('dev')
+    app.listen(3000) // start app listening on port 3000
+})
+// ------------------------------ //
+
+
+app.get('/', (req, res) => {
+    res.send("CryptoAnalytics API!")
+})
+
+app.get('/Subreddits', (req, res) => {
+    db.collection('subreddits').find().project({
+        "one_day_change":1,
+        "one_day_total":1,
+        "seven_day_change":1,
+        "seven_day_total":1,
+        "thirty_day_change":1,
+        "thirty_day_total":1
+    }).toArray(function(err, result) {
+        res.send(result)
+    }) 
+})
+
+//Use this cors config for production, allowing authorized origins to connect
+// app.use(function (req, res, next) {
+//     // Website you wish to allow to connect
+//     var allowedOrigins = ['http://localhost:3000', "http://api.cryptoanalytics.ml"];
+//     var origin = req.headers.origin;
+//     if (allowedOrigins.indexOf(origin) > -1) {
+//         res.setHeader('Access-Control-Allow-Origin', origin);
+//     }
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', false);
+//     // Pass to next layer of middleware
+//     next();
+// });
+
+module.exports = app;
