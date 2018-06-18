7e523a4abf854f947bebc0942bbffcecd9116dcf
Dylan Kilkenny <dylankilkenny95@gmail.com>
2018-06-18 16:24:42 +0100

    layout changes

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,9 +1,12 @@
 import 'semantic-ui-css/semantic.min.css';
+import 'react-github-button/assets/style.css';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Menu from "./js/components/presentational/Menu";
+import Modal from "./js/components/presentational/Modal";
 import TableContainer from "./js/components/container/TableContainer";
-import { Grid, Segment } from 'semantic-ui-react'
+import { Grid, Segment, Header, Icon, Button } from 'semantic-ui-react'
+import GitHubButton from 'react-github-button';
 
 
 class App extends React.Component {
@@ -12,14 +15,30 @@ class App extends React.Component {
             <div>
                 <Grid columns='equal'>
                     <Grid.Row>
-                        <Grid.Column>
-                        </Grid.Column>
-                        <Grid.Column textAlign='center' width={12}>
-                            <h1 style={{ color: '#096dd9' }}>
-                                CryptoSubs
-                            </h1>
-                        </Grid.Column>
-                        <Grid.Column>
+                        <Grid.Column textAlign='center' >
+                            <Segment>
+                                <Grid verticalAlign='middle' columns='equal'>
+                                    <Grid.Row>
+                                        <Grid.Column>
+                                        </Grid.Column>
+                                        <Grid.Column>
+                                        </Grid.Column>
+                                        <Grid.Column width={6}>
+                                            <Header as='h2' textAlign='center'>
+                                                CryptoCurrency Subreddit Tracker
+                                            </Header>
+                                        </Grid.Column>
+                                        <Grid.Column>
+                                            <Modal />
+                                            
+                                        </Grid.Column>
+                                        <Grid.Column>
+                                            <GitHubButton type="stargazers" size="large" namespace="dylankilkenny" repo="cryptosub" />
+                                            
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                </Grid>
+                            </Segment>
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row>
@@ -38,4 +57,7 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
 
