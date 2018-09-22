3a785d732c2d376b3f49ae013e8156ea152291eb
Dylan Kilkenny <dylankilkenny95@gmail.com>
2018-09-22 17:16:51 +0100

    changed menu and added moneybutton

--- webapp/src/js/components/presentational/HeaderSegment.js
+++ webapp/src/js/components/presentational/HeaderSegment.js
@@ -1,59 +1,62 @@
 import React from 'react';
-import { Segment, Grid, Header, Button, Icon } from 'semantic-ui-react';
+import { Header, Button, Icon, Menu } from 'semantic-ui-react';
 import GitHubButton from 'react-github-button';
-// import MoneyButton from 'react-money-button';
+import MoneyButton from '@moneybutton/react-money-button';
 import Modal from './Modal';
 import { Link } from 'react-router-dom';
 
-const HeaderSegment = props => (
-  <Segment>
-    <Grid verticalAlign="middle" columns="equal">
-      <Grid.Row>
-        <Grid.Column>
-          {window.location.pathname != '/' ? (
-            <Link to={`/`}>
-              <Icon
-                size="big"
-                color="yellow"
-                name="arrow alternate circle left outline"
-              />
-            </Link>
-          ) : null}
-        </Grid.Column>
-        <Grid.Column>
-          <Link to={`/Compare`}>
-            <Button size="small" color="yellow" content="Compare Activity" />
-          </Link>
-          {/* <MoneyButton
-            to={}
-            amount={1}
-            currency="EUR"
-            label="€1 Donate"
-            hideAmount={false}
-            clientIdentifier=""
-            buttonId=""
-            type="tip"
-          /> */}
-        </Grid.Column>
-        <Grid.Column mobile={4} width={6}>
-          <Header as="h2" textAlign="center">
-            Crypto Subreddit Tracker
-          </Header>
-        </Grid.Column>
-        <Grid.Column>
-          <Modal />
-        </Grid.Column>
-        <Grid.Column>
-          <GitHubButton
-            type="stargazers"
-            size="large"
-            namespace="dylankilkenny"
-            repo="cryptosub"
-          />
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  </Segment>
-);
+export default class HeaderSegment extends React.Component {
+  state = { activeItem: 'home' };
 
-export default HeaderSegment;
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <div>
+        <Menu secondary>
+          <Menu.Item>
+            <Header as="h2" textAlign="center">
+              <Icon name="reddit square" size="mini" />
+              Crypto Subreddit Tracker
+            </Header>
+          </Menu.Item>
+          <Menu.Item as={Link} to="/">
+            <Button size="small" color="yellow" content="Home" />
+          </Menu.Item>
+          <Menu.Item as={Link} to="/Compare">
+            <Button size="small" color="yellow" content="Compare Activity" />
+          </Menu.Item>
+          <Menu.Item>
+            <Modal />
+          </Menu.Item>
+
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <GitHubButton
+                type="stargazers"
+                size="large"
+                namespace="dylankilkenny"
+                repo="cryptosub"
+              />
+            </Menu.Item>
+            <Menu.Item position="right">
+              <MoneyButton
+                to="54"
+                amount="0.002"
+                currency="BCH"
+                label="€1 Donate"
+                hideAmount={false}
+                clientIdentifier="5af02e253b87ef334b081d74e4182b1b"
+                buttonId="1537026561224"
+                type="tip"
+                size="small"
+              />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+      </div>
+    );
+  }
+}
